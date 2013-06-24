d58bc3e8dd9bb1505964d468976967547d5af1af
iD Student <Student@scs-idua-strn-09.stanford.edu>
2013-06-24 10:14:56 -0700

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by iD Student on 6/24/13.
+//  Copyright (c) 2013 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
